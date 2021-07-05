8f572e250798176311bdf1ff4eb6509b6b7a1171
Chubby-Dev <anhnt2989@gmail.com>
2021-07-06 00:30:10 +0700

    [News Item] - Edited: layout

--- src/components/NewsItem/Wrapper.js
+++ src/components/NewsItem/Wrapper.js
@@ -7,6 +7,7 @@ const Wrapper = styled.div`
     width: 100%;
     height: 100%;
     max-height: 300px;
+    min-height: 300px;
     display: flex;
     justify-content: center;
     img {
--- src/components/NewsItem/index.js
+++ src/components/NewsItem/index.js
@@ -1,15 +1,15 @@
 import React, { memo } from 'react';
 import { Row, Col } from 'reactstrap';
 import clsx from 'clsx';
-import parse from 'html-react-parser';
+// import parse from 'html-react-parser';
 import dayjs from 'dayjs';
-import LinesEllipsis from 'react-lines-ellipsis'
+import LinesEllipsis from 'react-lines-ellipsis';
 
 import appConfig from 'constants/appConfig';
 import Wrapper from './Wrapper';
 
 function NewsItem(props) {
-  const { logo, slug, created_at, title, content, isStuck } = props;
+  const { logo, slug, created_at, title, summary, isStuck } = props;
   return (
     <Wrapper>
       <Row>
@@ -22,7 +22,18 @@ function NewsItem(props) {
           <div className="text-block">
             <p className={clsx("date", isStuck && "stuck")}>{created_at && dayjs(created_at).format('DD/MM/YYYY')}</p>
             <h5 className={clsx("title", isStuck && "stuck")}>{title && title}</h5>
-            {isStuck && <div className={clsx("text", isStuck && "stuck")}>{content && parse(content)}</div>}
+            {isStuck && 
+              <div className={clsx("text", isStuck && "stuck")}>
+                { summary && 
+                  <LinesEllipsis
+                    text={summary}
+                    maxLine={4}
+                    ellipsis="..."
+                    basedOn="words"
+                  />
+                }
+              </div>
+            }
           </div>
         </Col>
       </Row>
